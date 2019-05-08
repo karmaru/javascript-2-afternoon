28ef1da4418590bac5c98f1cf5e0c09614788a81
Todd Hipps <toddhipps@Todds-MacBook-Pro.local>
2019-05-08 14:03:27 -0600

    half through save

--- practice.js
+++ practice.js
@@ -15,7 +15,11 @@
 */
 
 //Code here
-
+var me = {
+  name: "Todd",
+  age: 46
+}
+alert(me)
 ////////// PROBLEM 2 //////////
 
 /*
@@ -24,21 +28,30 @@
 */
 
 //Code here
-
+var favoriteThings = {
+  band: "Nirvana",
+  food: "Peanut Butter",
+  person: "Gibson",
+  book: "Stranger in a Strange Land",
+  movie: "Star Wars",
+  holiday: "Halloween"
+}
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
 //Code here
-
+favoriteThings.car = "Mustang"
+favoriteThings.brand = "Adidas"
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
-
+favoriteThings.food = "Chicken Nuggets"
+favoriteThings.book = "Harry Potter"
 ////////// PROBLEM 3 //////////
 
 /*
@@ -50,13 +63,19 @@
 */
 
 //Code here
+var backPack = {
+}
+
+var item = "firstPocket"
+
+backPack[item] = "chapstick"
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
-
+alert (backPack)
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -64,7 +83,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
-
+console.log(backPack)
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -85,7 +104,8 @@ var user2 = {
 */
 
 //Code Here
-
+user2.name = "Bryan G. Smith"
+user2.email = "bryan.smith@devmounta.in"
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
@@ -96,14 +116,15 @@ var user2 = {
 */
 
 //Code Here
-
+var methodCollection = {}
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
 
 //Code Here
-
+methodCollection.alertHello = function() {alert("hello")}
+methodCollection.logHello = function() {console.log("hello")}
 /*
   Now call your alertHello and logHello methods.
 */
